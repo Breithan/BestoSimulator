1749a64c5523f1c3e1be319969ac16eed3422f19
Breithan <32719925+Breithan@users.noreply.github.com>
2018-10-09 13:05:51 -0500

    V1.4
    Clibrando

---
+++ Assets/Objects/Wind/Wind.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour {
+
+	public Vector3 windDirection;
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "Airplane") {
+			collision.gameObject.GetComponent<AirplanePhysical>().runningWind = windDirection;
+		}
+	}
+}
